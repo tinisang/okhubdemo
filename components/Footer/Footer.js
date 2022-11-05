550de3808cae5d96aefc099808a8a6ed236e7f3c
unknown <spham8011@gmail.com>
2022-11-05 10:44:28 +0700

    Add Basic Project Layout

---
+++ components/Footer/Footer.js
@@ -0,0 +1,9 @@
+export const Footer = ()=>{
+    return (
+        <>
+            <div className="footer">
+                Footer
+            </div>
+        </>
+    )
+}
---
+++ components/Header/Header.js
@@ -0,0 +1,9 @@
+export const Header =()=>{
+    return (
+        <>
+            <div className="header">
+                Header
+            </div>
+        </>
+    )
+}
