65cb183ad95f6d24880ba25f7b485dd3a10ce6a3
unknown <spham8011@gmail.com>
2022-11-07 17:47:18 +0700

    Sang update Build

--- components/HOME Components/Iconic Projects/IconicSlide.js
+++ components/HOME Components/Iconic Projects/IconicSlide.js
@@ -1,3 +1,7 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import image1 from '../../../public/imgs/image1.jpg'
+
 export const IconicSlide = ()=>{
     return (
         <>
@@ -8,6 +12,16 @@ export const IconicSlide = ()=>{
                         Dự án tiêu biểu
                     </div>
                     <div className="heading-english">Iconic Projects</div>
+                    <div className="projects-slide">
+                        <div className="project-item">
+                            <div className="image-container image-project">
+                                    <Link href="/">
+                                    <Image src={image1} alt='' fill className='image-item' />
+
+                                    </Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
