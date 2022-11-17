6dac1b74d990aaa1067e7b864e3d517b74d6fd66
Hai <haiek55tc1@gmail.com>
2022-11-17 09:10:50 +0700

    change er

--- components/NewsDetail/NewsDetail.jsx
+++ components/NewsDetail/NewsDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper";
 import "swiper/css";
