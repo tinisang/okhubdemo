62dced7f5492bf5ab06c5ab02cee8c6e5ac8b54d
unknown <spham8011@gmail.com>
2022-12-20 10:55:43 +0700

    Update Expansion Section

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,8 @@ export default function Home() {
     <>
        <div  >
 
- <ExpandSection/>
+      {!isMobile && <ExpandSection/> }
+
       {
         isMobile == false ? <IconicSlide/> : <IconSlideMobile/>
       }
